acc695d5031c1af6d7b3b4f3606d77af88259cec
karanOO89 <gk.987680@gmail.com>
2021-07-21 14:01:37 -0600

    added google login feature

--- client/main.jsx
+++ client/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import App from "../imports/ui/App";
+import  'meteor/accounts-ui'
+
 
 Meteor.startup(() => {
   render(
--- imports/ui/LoginForm.jsx
+++ imports/ui/LoginForm.jsx
@@ -1,8 +1,9 @@
 import { Meteor } from "meteor/meteor";
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./LoginForm.scss";
-
+import GoogleLogin, { googleLogin } from "react-google-login";
+import { FcGoogle } from "react-icons/fc";
 export const LoginForm = () => {
   const history = useHistory();
   const [username, setUsername] = useState("");
@@ -19,6 +20,45 @@ export const LoginForm = () => {
       }
     });
   };
+  const googleLoginSuccess = (response) => {
+    const googleEmail = response.profileObj.email;
+    const password = response.profileObj.googleId;
+    const email = googleEmail + " - Google a/c";
+
+    Accounts.createUser(
+      {
+        email: email,
+        password: password,
+      },
+      (err) => {
+        if (err) {
+          if (err.error === 403) {
+            Meteor.loginWithPassword(email, password, (err) => {
+              if (err) {
+                alert(err.message);
+              } else {
+                history.push("/info");
+              }
+            });
+          } else {
+            alert(err.message);
+          }
+        } else {
+          Meteor.loginWithPassword(email, password, (err) => {
+            if (err) {
+              alert(err.message);
+            } else {
+              history.push("/info");
+            }
+          });
+        }
+      }
+    );
+  };
+  const googleLoginFailure = (e) => {
+    // console.log("failed", response);
+    // console.log(response.profileObj);
+  };
   const clickHandler = () => {
     history.push("/register");
   };
@@ -27,7 +67,7 @@ export const LoginForm = () => {
       <form onSubmit={submit} className="login-form">
         <div>
           <input
-            type="text"
+            type="email"
             placeholder="Username"
             name="username"
             required
@@ -43,9 +83,26 @@ export const LoginForm = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button type="submit">Log In</button>
+        <button type="submit">Login</button>
       </form>
-      <button onClick={clickHandler}>Registration Page</button>
+      <div className="registerBtns">
+        <GoogleLogin
+          clientId="678569978220-2rchfru3m2vh3rmck48sf3t4mrubqobu.apps.googleusercontent.com"
+          render={(renderProps) => (
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+              style={{ color: "white" ,display:"flex",alignItems:"center",justifyContent:"center"}}
+            >
+              <FcGoogle size="25px"/>Login
+            </button>
+          )}
+          onSuccess={googleLoginSuccess}
+          onFailure={googleLoginFailure}
+          cookiePolicy={"single_host_origin"}
+        />
+        <button onClick={clickHandler}>Registration Page</button>
+      </div>
     </div>
   );
 };
